797aa99b70549ea08e9f2676ae96ba8960fd1f80
Vladimir <kozinskytxc@gmail.com>
2022-09-16 11:39:52 +0400

    feat: add signup endpoint

--- my-app-back/routers/Auth.js
+++ my-app-back/routers/Auth.js
@@ -20,4 +20,26 @@ router.post('/auth', async (req, res) => {
     }
 })
 
+router.post('/signup', async (req, res) => {
+    try {
+        const { email, password, firstName, lastName, position } = req.body;
+        const user = await User.findOne({ email: email }).exec();
+        if (!user) {
+            const newUser = await User.create({ email, password, firstName, lastName, position })
+
+            if (!newUser) throw new Error(`Account has not been created`);
+
+            res.statusMessage = "Account successfully created";
+            res.json({ message: "Account successfully created" });
+        } else {
+            throw new Error(`User with ${user.email} already exists`);
+        }
+    } catch (error) {
+        res.statusCode = 403;
+        res.statusMessage = error.message;
+        res.json({ message: error.message })
+    }
+})
+
+
 export default router;
