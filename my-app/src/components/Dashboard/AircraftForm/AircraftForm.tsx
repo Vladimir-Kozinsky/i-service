58f6a5cc5815a0143e91115ad5b9d1e6a555ade3
Vladimir <kozinskytxc@gmail.com>
2022-09-17 13:44:09 +0400

    feat: create aircraftForm

---
+++ my-app/src/components/Dashboard/AircraftForm/AircraftForm.tsx
@@ -0,0 +1,11 @@
+import s from './AircraftForm.module.scss';
+
+const AircraftForm = () => {
+    return (
+        <div>
+            Aircraft Form
+        </div>
+    )
+}
+
+export default AircraftForm;
