5091b0da942e590135ce553b407d0f26cb18f2a0
Vladimir Kozinsky <kozinskytxc@gmail.com>
2022-09-18 20:56:41 +0400

    fix: addAircraftForm

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,6 +3,7 @@ import Auth from './components/Auth/Auth';
 import { Routes, Route, } from "react-router-dom";
 import SignUp from './components/SignUp/SignUp';
 import Dashboard from './components/Dashboard/Dashboard';
+import AircraftForm from './components/Dashboard/AircraftForm/AircraftForm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/auth' element={<Auth />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/aircraft/add' element={<AircraftForm />} />
       </Routes>
     </div>
   );
