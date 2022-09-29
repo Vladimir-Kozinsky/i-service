69091a6c13c1b0f45d93ae8a63f49fc53400dce4
Vladimir <kozinskytxc@gmail.com>
2022-09-29 14:54:37 +0400

    feat: add aircraft info, buttons to legs

--- my-app/src/components/Legs/Legs.tsx
+++ my-app/src/components/Legs/Legs.tsx
@@ -1,9 +1,15 @@
 import { Field, Form, Formik, FormikHelpers } from "formik";
 import Button from "../../common/buttons/Button";
 import Input from "../../common/Input";
+import { IAircraft } from "../../store/reducers/aircraftReducer";
 import s from "./Legs.module.scss"
 import Pagenator from "./Pagenator/Pagenator";
+import backgroundImg1 from "./../../assets/img/png/back-img1.png"
+import backgroundImg2 from "./../../assets/img/png/back-img2.png"
 
+type ILegsProps = {
+    aircraft?: IAircraft;
+}
 interface IFilterValues {
     from: string;
     to: string;
@@ -40,9 +46,72 @@ const Leg: ILeg = {
     fc: '343433'
 }
 
+const fakeArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+const legs = fakeArr.map(() => {
+    return (
+        <div className={s.leg}>
+            <div className={s.leg__title__value}>{Leg.depDate}</div>
+            <div className={s.leg__title__value}>{Leg.flightNumber}</div>
+            <div className={s.leg__title__value}>{Leg.from}</div>
+            <div className={s.leg__title__value}>{Leg.to}</div>
+            <div className={s.leg__title__value}>{Leg.blockOff}</div>
+            <div className={s.leg__title__value}>{Leg.takeOff}</div>
+            <div className={s.leg__title__value}>{Leg.landing}</div>
+            <div className={s.leg__title__value}>{Leg.blockOn}</div>
+            <div className={s.leg__title__value}>{Leg.flightTime}</div>
+            <div className={s.leg__title__value}>{Leg.blockTime}</div>
+            <div className={s.leg__title__value}>{Leg.fh}</div>
+            <div className={s.leg__title__value}>{Leg.fc}</div>
+        </div>
+    )
+})
+
+const fakeAircraft = {
+    _id: "632ac45eaaac3b3162a6d242",
+    type: "Boeing 737-300",
+    regNum: "23233",
+    msn: "111111",
+    fh: "4444:11",
+    fc: "1111",
+    engines: [
+        { _id: "ac45eaaac3b3162a6d242pos", pos: 1, msn: "34343" },
+        { _id: "ac45eaaac3b3162a6d242pos", pos: 2, msn: "34343" },
+        { _id: "ac45eaaac3b3162a6d242pos", pos: 3, msn: "34343" },
+        { _id: "ac45eaaac3b3162a6d242pos", pos: 4, msn: "34343" }
+    ],
+    apu: "111",
+    legs: []
+}
+
 const Legs = () => {
     return (
         <div className={s.legs__contaiter}>
+              <div className={s.background__circle}></div>
+              <img className={s.background__img1} src={backgroundImg1} alt="" />
+              <img className={s.background__img2} src={backgroundImg2} alt="" />
+            <div className={s.aircraftInfo} >
+                <div className={s.aircraftInfo__block} >
+                    <span className={s.aircraftInfo__block__title}>Type:</span>
+                    <span className={s.aircraftInfo__block__value}>{fakeAircraft.type}</span>
+                </div>
+                <div className={s.aircraftInfo__block}>
+                    <span className={s.aircraftInfo__block__title}>MSN:</span>
+                    <span className={s.aircraftInfo__block__value}>{fakeAircraft.msn}</span>
+                </div>
+                <div className={s.aircraftInfo__block}>
+                    <span className={s.aircraftInfo__block__title}>Reg:</span>
+                    <span className={s.aircraftInfo__block__value}>{fakeAircraft.regNum}</span>
+                </div>
+                <div className={s.aircraftInfo__block}>
+                    <span className={s.aircraftInfo__block__title}>FH:</span>
+                    <span className={s.aircraftInfo__block__value}>{fakeAircraft.fh}</span>
+                </div>
+                <div className={s.aircraftInfo__block}>
+                    <span className={s.aircraftInfo__block__title}>FC:</span>
+                    <span className={s.aircraftInfo__block__value}>{fakeAircraft.fc}</span>
+                </div>
+            </div>
             <Formik
                 initialValues={{
                     from: '',
@@ -64,7 +133,7 @@ const Legs = () => {
                         <label>To</label>
                         <Input type='date' id='to' name='to' placeholder='' />
                     </div>
-                    <Button text="Search" color="green" btnType="submit" />
+                    <Button text="Search" color="white" btnType="submit" />
                 </Form>
             </Formik>
             <Pagenator />
@@ -83,20 +152,11 @@ const Legs = () => {
                     <div className={s.leg__title__value}>FH</div>
                     <div className={s.leg__title__value}>FC</div>
                 </div>
-                <div className={s.leg}>
-                    <div className={s.leg__title__value}>{Leg.depDate}</div>
-                    <div className={s.leg__title__value}>{Leg.flightNumber}</div>
-                    <div className={s.leg__title__value}>{Leg.from}</div>
-                    <div className={s.leg__title__value}>{Leg.to}</div>
-                    <div className={s.leg__title__value}>{Leg.blockOff}</div>
-                    <div className={s.leg__title__value}>{Leg.takeOff}</div>
-                    <div className={s.leg__title__value}>{Leg.landing}</div>
-                    <div className={s.leg__title__value}>{Leg.blockOn}</div>
-                    <div className={s.leg__title__value}>{Leg.flightTime}</div>
-                    <div className={s.leg__title__value}>{Leg.blockTime}</div>
-                    <div className={s.leg__title__value}>{Leg.fh}</div>
-                    <div className={s.leg__title__value}>{Leg.fc}</div>
-                </div>
+                {legs}
+            </div>
+            <div className={s.buttons} >
+                <Button text="Back" btnType="button" color="white" />
+                <Button text="Add" btnType="button" color="green" />
             </div>
         </div>
 
