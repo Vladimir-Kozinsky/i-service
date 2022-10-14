1751ee7ba3083ec5741c829d771f81038d5f2c7b
Vladimir <kozinskytxc@gmail.com>
2022-10-14 14:05:46 +0400

    feat: add engine widget

--- my-app/src/components/Engines/EngineWidget/EngineWidget.tsx
+++ my-app/src/components/Engines/EngineWidget/EngineWidget.tsx
@@ -1,9 +1,12 @@
-import s from './EngineWidget.module.scss'
+import s from './EngineWidget.module.scss';
+import engineImg from './../../../assets/img/png/engine-widget-icon.png';
 
-const EngineWidget: React.FC = () => {
+const EngineWidget: React.FC<any> = ({ engine }) => {
     return (
         <div className={s.engineWidget}>
-            engine widget
+            <h3 className={s.engineWidget__header} >MSN: 25089</h3>
+            <div></div>
+            <img className={s.engineWidget__img} src={engineImg} alt="icon" />
         </div>
     )
 }
--- my-app/src/components/Engines/Engines.tsx
+++ my-app/src/components/Engines/Engines.tsx
@@ -2,14 +2,30 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '../../common/buttons/Button';
 import s from './Engines.module.scss';
+import EngineWidget from './EngineWidget/EngineWidget';
+import addEngineImg from '../../assets/img/png/cross.png'
 
+const fakeEngines = [
+    { _id: 'sdfsdf', msn: '25897', fh: '2569:33', fc: '45859', lastRepData: '25-05-1989', lastRepFh: '2323:33', lastRepFc: "51465" },
+    { _id: 'sdfsdf', msn: '25897', fh: '2569:33', fc: '45859', lastRepData: '25-05-1989', lastRepFh: '2323:33', lastRepFc: "51465" },
+    { _id: 'sdfsdf', msn: '25897', fh: '2569:33', fc: '45859', lastRepData: '25-05-1989', lastRepFh: '2323:33', lastRepFc: "51465" },
+    { _id: 'sdfsdf', msn: '25897', fh: '2569:33', fc: '45859', lastRepData: '25-05-1989', lastRepFh: '2323:33', lastRepFc: "51465" },
+    { _id: 'sdfsdf', msn: '25897', fh: '2569:33', fc: '45859', lastRepData: '25-05-1989', lastRepFh: '2323:33', lastRepFc: "51465" },
+    { _id: 'sdfsdf', msn: '25897', fh: '2569:33', fc: '45859', lastRepData: '25-05-1989', lastRepFh: '2323:33', lastRepFc: "51465" },
+]
 
 const Engines: React.FC = () => {
     const navigate = useNavigate();
+    const engines = fakeEngines.map((engine: any) => {
+        return <EngineWidget engine={engine} />
+    })
     return (
         <>
             <div className={s.engines}>
-
+                {engines}
+                <div className={s.widget}>
+                    <img className={s.widget__img} src={addEngineImg} alt="" />
+                </div>
             </div>
             <Button text='Back' color='white__dark' btnType='button' handler={() => navigate('/dashboard')} />
         </>
