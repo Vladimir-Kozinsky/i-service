8baea426bb1170d66befcecbd4309374c4486c80
Vladimir Kozinsky <kozinskytxc@gmail.com>
2023-01-06 17:55:18 +0400

    refact: feedback form

--- my-app/src/components/Main/Feedback/Feedback.tsx
+++ my-app/src/components/Main/Feedback/Feedback.tsx
@@ -1,62 +1,73 @@
-import { Field, Form, Formik } from 'formik';
+import { Field, Form, Formik } from 'formik'
 import React from 'react'
 import s from './Feedback.module.scss'
+import cross from '../../../assets/img/png/cross-input.png'
 
 type IFeedbackValues = {
     name: string;
-    number: string;
+    phone: string;
     company: string;
     message: string;
 }
 
 const Feedback: React.FC = () => {
     return (
-        <div className={s.message} >
-            <Formik
-                initialValues={{
-                    name: '',
-                    number: '',
-                    company: '',
-                    message: ''
-                }}
-                validate={values => {
-                    interface IFeedback {
-                        name?: string;
-                        number?: string;
-                        company?: string;
-                        message?: string;
-                    }
-                    const errors: IFeedback = {};
-                    if (!values.name) errors.name = 'Name is required';
-                    if (!values.number) errors.number = 'Email is required';
-                    if (!values.company) errors.company = 'Company is required';
-                    if (!values.message) errors.message = 'Message is required';
-                    return errors;
-                }}
-                onSubmit={(
-                    values: IFeedbackValues,
-                ) => {
-                    console.log(values)
-                }}
-            >{({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleBlur,
-                handleSubmit,
-            }) => (
-                <Form className={s.message}>
-                    <h3 className={s.message__title}>Напишите нам</h3>
-                    <Field type="text" id="name" name="name" placeholder="Ваше имя" error={errors.name} as='input' />
-                    <Field type="text" id="number" name="number" placeholder='Ваш телефон' error={errors.number} as='input' />
-                    <Field type="text" id="company" name="company" placeholder='Компания' error={errors.number} as='input' />
-                    <Field type="text" id="message" name="message" placeholder='Ваше сообщение' error={errors.number} as='textarea' />
-                    <button className={s.message__btn}>Отправить</button>
-                </Form>
-            )}
-            </Formik>
-        </div>
+        <Formik
+            initialValues={{
+                name: '',
+                phone: '',
+                company: '',
+                message: ''
+            }}
+            validate={values => {
+                interface IFeedback {
+                    name?: string;
+                    phone?: string;
+                    company?: string;
+                    message?: string;
+                }
+                const errors: IFeedback = {};
+                if (!values.name) errors.name = 'Name is required';
+                if (!values.phone) errors.phone = 'Email is required';
+                if (!values.company) errors.company = 'Company is required';
+                if (!values.message) errors.message = 'Message is required';
+                return errors;
+            }}
+            onSubmit={(
+                values: IFeedbackValues,
+            ) => {
+                console.log(values)
+            }}
+        >{({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+        }) => (
+            <Form className={s.message}>
+                <h3 className={s.message__title}>Напишите нам</h3>
+                <div className={s.message__field} >
+                    {errors.name && <img src={cross} alt='cross-img' />}
+                    <Field type="text" id="name" name="name" placeholder="Ваше имя" as='input' />
+                </div>
+                <div className={s.message__field} >
+                    {errors.phone && <img src={cross} alt='cross-img' />}
+                    <Field type="text" id="phone" name="phone" placeholder='Ваш телефон' as='input' />
+                </div>
+                <div className={s.message__field} >
+                    {errors.company && <img src={cross} alt='cross-img' />}
+                    <Field type="text" id="company" name="company" placeholder='Компания' as='input' />
+                </div>
+                <div className={s.message__field} >
+                    {errors.message && <img src={cross} alt='cross-img' />}
+                    <Field type="text" id="message" name="message" placeholder='Ваше сообщение' as='textarea' />
+                </div>
+                <button className={s.message__btn}>Отправить</button>
+            </Form>
+        )}
+        </Formik>
     )
 }
 
