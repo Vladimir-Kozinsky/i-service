1063a18c0972f192a3a96c0ac648c6e3f86be407
Vladimir Kozinsky <kozinskytxc@gmail.com>
2023-01-15 15:16:08 +0400

    refact: dashboard

--- my-app/src/components/Dashboard/Dashboard.tsx
+++ my-app/src/components/Dashboard/Dashboard.tsx
@@ -1,30 +1,34 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Header from "../Header/Header";
 import s from "./Dashboard.module.scss"
 import planeImg from "../../assets/img/png/plane-icon.png"
 import engineImg from "../../assets/img/png/engine-icon.png"
 import apuImg from "../../assets/img/png/apu.png"
 import classNames from "classnames";
-import Aircrafts from "../Aircrafts/Aircrafts";
-import Engines from "../Engines/Engines";
 import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../../store/store";
 import { setUser } from "../../store/reducers/authReducer";
-import Apus from "../Apus/Apus";
 import Loader from "../../common/Loader/Loader";
 import { Transition } from 'react-transition-group';
 
 const Dashboard: React.FC = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch<AppDispatch>();
-
+    const [isLoader, setIsLoader] = useState(false);
     useEffect(() => {
-        dispatch(setUser())
+        (async () => {
+            await setIsLoader(true)
+            await dispatch(setUser())
+            await setIsLoader(false)
+        })()
     }, []);
     return (
         <div className={s.dashboard} >
             <div className={s.dashboard__container}>
+                <Transition in={isLoader} timeout={400} unmountOnExit mountOnEnter >
+                    {(state) => <Loader state={state} />}
+                </Transition>
                 <Header />
                 <div className={s.dashboard__widgets} >
                     <div className={s.dashboard__widget} onClick={() => navigate('/dashboard/aircrafts')} >
